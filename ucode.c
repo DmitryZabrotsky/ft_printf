e357a8f09f52505c7c437512a1c679d5fbc3a7d6
DmitryZabrotsky <reezee512@gmail.com>
2018-04-15 23:58:25 +0300

    added ucode file with putwchar at the repo

--- ucode.c
+++ ucode.c
@@ -4,6 +4,32 @@
 #include <locale.h>
 #include <stdlib.h>
 
+//need func as wchar to char* 
+
+void	ft_putwchar(wchar_t chr)
+{
+	if (chr <= 0x7F)
+		ft_putchar(chr);
+	else if (chr <= 0x7FF)
+	{
+		ft_putchar((chr >> 6) + 0xC0);
+		ft_putchar((chr & 0x3F) + 0x80);
+	}
+	else if (chr <= 0xFFFF)
+	{
+		ft_putchar((chr >> 12) + 0xE0);
+		ft_putchar(((chr >> 6) & 0x3F) + 0x80);
+		ft_putchar((chr & 0x3F) + 0x80);
+	}
+	else if (chr <= 0x10FFFF)
+	{
+		ft_putchar((chr >> 18) + 0xF0);
+		ft_putchar(((chr >> 12) & 0x3F) + 0x80);
+		ft_putchar(((chr >> 6) & 0x3F) + 0x80);
+		ft_putchar((chr & 0x3F) + 0x80);
+	}
+}
+
 int main(void)
 {
 	setlocale(LC_ALL, "");
@@ -18,4 +44,6 @@ int main(void)
 	printf("\n  %i\n", printf("%ls", ws));
 
 	printf("%i\n", MB_CUR_MAX);
+
+	ft_putwchar(wc);
 }
