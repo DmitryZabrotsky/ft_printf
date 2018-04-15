6ab24b22fb5cb76f4761894c750ab3c539ad9127
DmitryZabrotsky <reezee512@gmail.com>
2018-04-16 00:28:22 +0300

    added wchartochar with out norm

--- ucode.c
+++ ucode.c
@@ -4,7 +4,7 @@
 #include <locale.h>
 #include <stdlib.h>
 
-//need func as wchar to char* 
+//need func as wchar to lschar* 
 
 void	ft_putwchar(wchar_t chr)
 {
@@ -30,10 +30,44 @@ void	ft_putwchar(wchar_t chr)
 	}
 }
 
+char	*ft_wchartochar(wchar_t chr)
+{
+	char *res;
+
+	res = (char *)malloc(sizeof(char) * 5);
+	if (chr <= 0x7F)
+	{
+		res[0] = chr;
+		res[1] = '\0';
+	}
+	else if (chr <= 0x7FF)
+	{
+		res[0] = (chr >> 6) + 0xC0;
+		res[1] = (chr & 0x3F) + 0x80;
+		res[2] = '\0';
+	}
+	else if (chr <= 0xFFFF)
+	{
+		res[0] = (chr >> 12) + 0xE0;
+		res[1] = ((chr >> 6) & 0x3F) + 0x80;
+		res[2] = (chr & 0x3F) + 0x80;
+		res[3] = '\0';
+	}
+	else if (chr <= 0x10FFFF)
+	{
+		res[0] = (chr >> 18) + 0xF0;
+		res[1] = ((chr >> 12) & 0x3F) + 0x80;
+		res[2] = ((chr >> 6) & 0x3F) + 0x80;
+		res[3] = (chr & 0x3F) + 0x80;
+		res[4] = '\0';
+	}
+	return (res);
+}
+
 int main(void)
 {
 	setlocale(LC_ALL, "");
-	//(♕ == 9813 == 0x2655 == 11100010 10011001 10010101 00001010)
+	//(♕ == 9813 == 0x2655)
 	wchar_t wc = (wchar_t)0x2655; 
 	wchar_t ws[3];
 	ws[0] = (wchar_t)0x2655;
@@ -46,4 +80,13 @@ int main(void)
 	printf("%i\n", MB_CUR_MAX);
 
 	ft_putwchar(wc);
+	ft_putchar('\n');
+	ft_putchar('\n');
+
+	char *str;
+	str = ft_wchartochar(wc);
+	ft_putstr(str);
+	ft_putchar('\n');
+	ft_putnbr(ft_strlen(str));
+	ft_putchar('\n');
 }
