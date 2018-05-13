bfdb10de09223f528d3b016109e6346159503884
DmitryZabrotsky <reezee512@gmail.com>
2018-05-13 18:33:54 +0300

    testing e on site

--- src/builders/e_builder.c
+++ src/builders/e_builder.c
@@ -3,26 +3,24 @@
 static int				f_to_e(long double *num)
 {
 	int					e;
-	printf("--------------- NUM: %Lf\n", *num);
+
 	if (*num == 0)
 		return (0);
 	//return (1);
 	e = 0;
 	while (1 > (intmax_t)(*num) || 10 <= (intmax_t)(*num))
 	{
-		if ((intmax_t)(*num) < 1)
-		{
-			*num *= 10.0;
-			e--;
-		}
+		// if ((intmax_t)(*num) < 1)
+		// {
+		// 	*num *= 10.0;
+		// 	e--;
+		// }
 		if ((intmax_t)(*num) >= 10)
 		{
 			*num /= 10.0;
 			e++;
 		}
-		printf("%Lf\n", *num);
 	}
-	printf("------------ END OF FUNC e: %d\n", e);
 	return (e);
 }
 
