300d70b049eb7b3ad546c3683c9520c051a052c3
DmitryZabrotsky <reezee512@gmail.com>
2018-05-13 16:59:06 +0300

    testing site

--- src/string_assembler.c
+++ src/string_assembler.c
@@ -36,36 +36,27 @@ char			*assemble_string(t_format *format, va_list args, t_flags *flags)
 {
 	check_format(format);
 	if (format->type == '%')
-		//return ("DZ <3 AC");
 		return (build_percent(format));
 	else if (format->type == 'c' || format->type == 'C')
-		//return ("DZ <3 AC");
 		return (build_c(format, args, flags));
 	else if (format->type == 's' || format->type == 'S')
-		//return ("DZ <3 AC");
 		return (build_s(format, args));
 	else if (format->type == 'd' || format->type == 'D' || format->type == 'i')
-		//return ("DZ <3 AC");
 		return (build_di(format, args));
 	else if (format->type == 'o' || format->type == 'O' ||
 			format->type == 'u' || format->type == 'U' ||
 			format->type == 'x' || format->type == 'X')
-		//return ("DZ <3 AC");
 		return (build_oux(format, args, flags));
 	else if (format->type == 'p')
-		//return ("DZ <3 AC");
 		return (build_p(format, args, flags));
 	else if (format->type == 'b')
-		//return ("DZ <3 AC");
 		return (build_oux(format, args, flags));
 	else if (format->type == 'f' || format->type == 'F')
-		return ("DZ <3 AC");
+		return (ft_strdup("DZAC"));
 		//return (build_f(format, args));
 	else if (format->type == 'e' || format->type == 'E')
-		//return ("DZ <3 AC");
 		return (build_e(format, args));
 	else if (format->type == 'a' || format->type == 'A')
-		//return ("DZ <3 AC");
 		return (build_a(format, args));
 	else
 		return (ft_strdup("")); //think about this!
